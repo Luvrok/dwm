5308e4790e38088a05f77e80a300c9c34a26405b
Luvrok <morturion.life@gmail.com>
2025-12-22 23:45:04 +0300

    fix some build errors

--- config.h
+++ config.h
@@ -1,19 +1,14 @@
-/*
-luvrok's dwm build
-
-patches applied (including some slight changes):
-
-*/
-
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int refreshrate        = 120;      /* refresh rate (per second) for client move/resize */
 
 static const char *fonts[]          = { "JetBrainsMonoNL NFP:size=12", "Font Awesome 6 Free Solid:size=12" };
-static const char dmenufont[]       = "JetBrainsMonoNL NFP:size=12";;
+static const char dmenufont[]       = "JetBrainsMonoNL NFP:size=12";
 
 /* default colors used if xrdb is not loaded */
 static char normbgcolor[]           = "#2e3440";
@@ -78,8 +73,7 @@ static const Layout layouts[] = {
 #define TERMINAL  "kitty"
 
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu", NULL };
 
 void spawn_with_lang_switch(const Arg *arg) {
     system("xkb-switch -s us && pkill -RTMIN+1 dwmblocks");  // change language to english
@@ -132,7 +126,7 @@ static const Key keys[  ] = {
   { MODKEY,                       XK_t,                     setlayout,              { .v = &layouts[0] } },
   { MODKEY,                       XK_f,                     setlayout,              { .v = &layouts[1] } },
   { MODKEY,                       XK_m,                     setlayout,              { .v = &layouts[2] } },
-  { MODKEY,                       XK_space,                 setlayout,              { 0 } },
+  // { MODKEY,                       XK_space,                 setlayout,              { 0 } },
   { MODKEY|ShiftMask,             XK_space,                 togglefloating,         { 0 } },
   { MODKEY,                       XK_0,                     view,                   { .ui = ~0 } },
   { MODKEY|ShiftMask,             XK_0,                     tag,                    { .ui = ~0 } },
