8a9d628260f235658fd1295664a0f36141143559
Luvrok <morturion.life@gmail.com>
2025-12-22 23:19:27 +0300

    add my config.h config

---
+++ config.h
@@ -0,0 +1,173 @@
+/*
+luvrok's dwm build
+
+patches applied (including some slight changes):
+
+*/
+
+
+/* appearance */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+
+static const char *fonts[]          = { "JetBrainsMonoNL NFP:size=12", "Font Awesome 6 Free Solid:size=12" };
+static const char dmenufont[]       = "JetBrainsMonoNL NFP:size=12";;
+
+/* default colors used if xrdb is not loaded */
+static char normbgcolor[]           = "#2e3440";
+static char normbordercolor[]       = "#4c566a";
+static char normfgcolor[]           = "#d8dee9";
+static char selfgcolor[]            = "#eceff4";
+static char selbordercolor[]        = "#a3be8c";
+static char selbgcolor[]            = "#b48ead";
+
+static const char *colors[][3]      = {
+  /*               fg         bg         border   */
+  [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
+  [SchemeSel]  = { selfgcolor, selbgcolor, selbordercolor },
+};
+
+/* tagging */
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+static const Rule rules[] = {
+  /* class             instance  title  tags mask   isfloating   monitor */
+  { "feh",             NULL,     NULL,  0,          1,          -1 },
+
+  { "firefox",         NULL,     NULL,  0,          0,          -1 },
+  { "obsidian",        NULL,     NULL,  0,          0,          -1 },
+  { "kitty",           NULL,     NULL,  0,          0,          -1 },
+
+  { "Spotify",         NULL,     NULL,  0,          0,          -1 },
+  { "qBittorrent",     NULL,     NULL,  1 << 8,     0,          -1 },
+
+  { "Element",         NULL,     NULL,  1 << 3,     0,          -1 },
+  { "TelegramDesktop", NULL,     NULL,  1 << 3,     0,          -1 },
+};
+
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster     = 1;    /* number of clients in master area */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+
+static const Layout layouts[] = {
+  /* symbol  arrange function */
+  { "󰓒", tile },      // тайлинг (основная раскладка по умолчанию)
+  { "󰇥", NULL },      // floating (NULL = плавающие окна)
+  { "", monocle },   // одно окно на весь экран
+};
+
+#define MODKEY Mod4Mask
+
+#define TAGKEYS(KEY,TAG) \
+{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
+{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
+{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
+{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+
+/* helper for spawning shell commands in the pre dwm-5.0 fashion */
+#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+
+/* helper for launching gtk application */
+#define GTKCMD(cmd) { .v = (const char*[]){ "/usr/bin/gtk-launch", cmd, NULL } }
+
+#define STATUSBAR "dwmblocks"
+#define BROWSER  "firefox"
+#define TERMINAL  "kitty"
+
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static const char *dmenucmd[] = { };
+static const char *termcmd[]  = { "st", NULL };
+
+void spawn_with_lang_switch(const Arg *arg) {
+    system("xkb-switch -s us && pkill -RTMIN+1 dwmblocks");  // change language to english
+    spawn(arg);                  // start dmenu_run
+}
+
+static const char *termcmd[]  = { "kitty", NULL };
+static const char *firefoxcmd[]  = { "select-profile-firefox--rofi", NULL };
+static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
+static const char *greenclipcmd[] = { "dwm-greenclip", NULL };
+static const char *lang_pkill[] = { "pkill", "-RTMIN+1", "dwmblocks", NULL};
+static const char *upbrt[] = {"dwm-brightness", "up", NULL};
+static const char *downbrt[] = {"dwm-brightness", "down", NULL};
+static const char *upvol[] = {"dwm-volume", "up", NULL};
+static const char *downvol[] = {"dwm-volume", "down", NULL};
+static const char *mutevol[] = {"dwm-volume", "mute", NULL};
+
+static const Key keys[  ] = {
+  /* modifier                     key                       function                argument */
+  { MODKEY,                       XK_p,                     spawn_with_lang_switch, SHCMD("j4-dmenu-desktop --dmenu='dmenu -i -m 0 -fn \"JetBrainsMonoNL NFP:size=12\" -nb \"#282828\" -nf \"#ebdbb2\" -sb \"#d65d0e\" -sf \"#fbf1c7\"' --term='kitty'") },
+  { MODKEY,                       XK_c,                     spawn,                  { .v = greenclipcmd } },
+  { MODKEY|ShiftMask,             XK_t,                     spawn,                  { .v = termcmd } },
+  { MODKEY|ShiftMask,             XK_f,                     spawn,                  { .v = firefoxcmd } },
+  { MODKEY,                       XK_space,                 spawn,                  { .v = lang_pkill } },
+  { MODKEY,                       XK_w,                     spawn,                  SHCMD("dwm-power_menu") },
+  { MODKEY,                       XK_u,                     spawn,                  SHCMD("rofi-pass") },
+
+  /* XF86Keys */
+  { 0,                            XF86XK_AudioMute,         spawn,                  { .v = mutevol}},
+  { 0,                            XF86XK_AudioLowerVolume,  spawn,                  { .v = downvol}},
+  { 0,                            XF86XK_AudioRaiseVolume,  spawn,                  { .v = upvol}},
+  { 0,                            XF86XK_MonBrightnessUp,   spawn,                  { .v = upbrt}},
+  { 0,                            XF86XK_MonBrightnessDown, spawn,                  { .v = downbrt}},
+  { 0,                            XK_Print,                 spawn,                  { .v = screenshotcmd } },
+
+  { MODKEY|Mod1Mask,              XK_c,                     spawn,                  SHCMD("dwm-center-window") },
+
+  { MODKEY,                       XK_b,                     togglebar,              { 0 } },
+  { MODKEY,                       XK_j,                     focusstack,             { .i = +1 } },
+  { MODKEY,                       XK_k,                     focusstack,             { .i = -1 } },
+  { MODKEY,                       XK_i,                     incnmaster,             { .i = +1 } }, /* increase numbers of master windows */
+  { MODKEY,                       XK_d,                     incnmaster,             { .i = -1 } }, /* decrease numbers of master windows */
+  { MODKEY,                       XK_h,                     setmfact,               { .f = -0.05  } }, /* decrease size of master windows */
+  { MODKEY,                       XK_l,                     setmfact,               { .f = +0.05  } }, /* increase size of master windows */
+
+  { MODKEY|ShiftMask,             XK_c,                     killclient,             { 0 } },
+  { MODKEY,                       XK_Return,                zoom,                   { 0 } },
+  { MODKEY,                       XK_Tab,                   view,                   { 0 } },
+
+  { MODKEY,                       XK_t,                     setlayout,              { .v = &layouts[0] } },
+  { MODKEY,                       XK_f,                     setlayout,              { .v = &layouts[1] } },
+  { MODKEY,                       XK_m,                     setlayout,              { .v = &layouts[2] } },
+  { MODKEY,                       XK_space,                 setlayout,              { 0 } },
+  { MODKEY|ShiftMask,             XK_space,                 togglefloating,         { 0 } },
+  { MODKEY,                       XK_0,                     view,                   { .ui = ~0 } },
+  { MODKEY|ShiftMask,             XK_0,                     tag,                    { .ui = ~0 } },
+
+  { MODKEY,                       XK_comma,                 focusmon,               { .i = -1 } },
+  { MODKEY,                       XK_period,                focusmon,               { .i = +1 } },
+  { MODKEY|ShiftMask,             XK_comma,                 tagmon,                 { .i = -1 } },
+  { MODKEY|ShiftMask,             XK_period,                tagmon,                 { .i = +1 } },
+
+  TAGKEYS(                        XK_1,                     0)
+  TAGKEYS(                        XK_2,                     1)
+  TAGKEYS(                        XK_3,                     2)
+  TAGKEYS(                        XK_4,                     3)
+  TAGKEYS(                        XK_5,                     4)
+  TAGKEYS(                        XK_6,                     5)
+  TAGKEYS(                        XK_7,                     6)
+  TAGKEYS(                        XK_8,                     7)
+  TAGKEYS(                        XK_9,                     8)
+
+  { MODKEY|ShiftMask,             XK_q,                     quit,                   { 0 } },
+};
+
+/* button definitions */
+/* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
+static const Button buttons[] = {
+  /* click                event mask      button          function        argument */
+  { ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+  { ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+  { ClkWinTitle,          0,              Button2,        zoom,           {0} },
+  { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
+  { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
+  { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
+  { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+  { ClkTagBar,            0,              Button1,        view,           {0} },
+  { ClkTagBar,            0,              Button3,        toggleview,     {0} },
+  { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+  { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
