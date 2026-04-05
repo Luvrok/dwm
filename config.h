b93de7896c5878c1872ecb4296847a7e64a4ece3
Luvrok <morturion.life@gmail.com>
2026-04-05 16:45:12 +0300

    add floating rule for android-webcam(ffplay) and add keybind for it

--- config.h
+++ config.h
@@ -98,8 +98,9 @@ static const Rule rules[] = {
   { "TelegramDesktop", WTYPE "UTILITY", NULL, NULL, 0,        1,           0,           NULL,      -1,        0,            0 },
   { "Zathura",         NULL,     NULL,     NULL,  0,          1,           0,           "-12X 10Y 720W 900H", -1, 0,        0 },
   { "nixos_menu_log",  NULL,     NULL,     NULL,  0,          1,           0,           NULL,      -1,        0,            0 },
-  { "Dragon-drop",     NULL,     NULL,     NULL,  0,          1,           1,           NULL,      -1,        -1,            0 },
-  { "st-256color",     NULL,     "spterm", "scratchpad", 0,   1,           1,           "1280W 1200H",-1,     -1,            's' },
+  { "Dragon-drop",     NULL,     NULL,     NULL,  0,          1,           1,           NULL,      -1,        -1,           0 },
+  { "st-256color",     NULL,     "spterm", "scratchpad", 0,   1,           1,           "1280W 1200H",-1,     -1,           's' },
+  { "ffplay",          NULL,     NULL,     "android-webcam", 0, 1,         0,           NULL,      -1,        -1,            0 },
 };
 
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
@@ -170,13 +171,14 @@ static const Key keys[  ] = {
   { MODKEY|ShiftMask,             XK_f,                     spawn,                  { .v = librewolfcmd } },
   { MODKEY,                       XK_space,                 spawn,                  { .v = lang_pkill } },
   { MODKEY,                       XK_w,                     spawn,                  SHCMD("dwm-power_menu") },
+  { MODKEY|ShiftMask,             XK_w,                     spawn,                  SHCMD("webcam-start") },
   { MODKEY,                       XK_r,                     spawn,                  SHCMD("rofi-recording") },
   { MODKEY|ShiftMask,             XK_r,                     spawn,                  SHCMD("rofi-recording stop") },
   { MODKEY,                       XK_u,                     spawn,                  SHCMD("rofi-pass") },
   { MODKEY|ControlMask,           XK_k,                     spawn,                  SHCMD("rofi-killer") },
   { MODKEY|ShiftMask,             XK_u,                     spawn,                  SHCMD("nixos_menu") },
   { MODKEY|ShiftMask,             XK_d,                     spawn,                  SHCMD("audioswitcher") },
-  { MODKEY|ShiftMask,             XK_p,                     floatpos,               { .v = "-10X -10Y 720W 400H" } },
+  { MODKEY|ShiftMask,             XK_p,                     floatpos,               { .v = "-10X -10Y" } },
 
 	{ MODKEY,                       XK_g,                     togglescratch,          { .v = scratchpadcmd } },
 	{ MODKEY|ShiftMask,             XK_g,                     removescratch,          { .v = scratchpadcmd } },
