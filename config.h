09a5d8c28a21fa73f8d80a71dbe090be28312eee
Luvrok <pawel.2020.navtop@gmail.com>
2026-06-01 18:58:53 +0300

    fix(keys): rebind movecenter shortcut to avoid librewolf key conflict

--- config.h
+++ config.h
@@ -221,7 +221,7 @@ static const Key keys[  ] = {
   { MODKEY,                       XK_period,                focusmon,               { .i = +1 } },
   { MODKEY|ShiftMask,             XK_comma,                 tagmon,                 { .i = -1 } },
   { MODKEY|ShiftMask,             XK_period,                tagmon,                 { .i = +1 } },
-  { MODKEY|ShiftMask,             XK_f,                     movecenter,             { 0 } },
+  { MODKEY,                       XK_f,                     movecenter,             { 0 } },
   { MODKEY,                       XK_F5,                    xrdb,                   { .v = NULL } },
   // { MODKEY,                       XK_s,                     show,                   { 0 } },
   { MODKEY|ControlMask,           XK_s,                     showall,                { 0 } },
